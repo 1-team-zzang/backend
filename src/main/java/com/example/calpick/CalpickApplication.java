8771bb8ac2a996f35b0406ba8bc031438410e028
이다은 <llynn97@naver.com>
2025-07-12 20:59:29 +0900

    init: 초기 설정

---
+++ src/main/java/com/example/calpick/CalpickApplication.java
@@ -0,0 +1,13 @@
+package com.example.calpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/calpick/CalpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
