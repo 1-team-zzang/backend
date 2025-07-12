21b703213ace85bf98c2b9a2910e73d5eb95b6df
shinyurim <yoursin0330@gmail.com>
2025-07-12 21:59:22 +0900

    feat/entity 생성

---
+++ src/main/java/com/example/calpick/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.calpick.domain;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    @Id
+    private Long userId;
+    private String email;
+    private String password;
+    private String name;
+    private String profile_url;
+    private String user_status;
+    private String login_type;
+    private LocalDateTime created_at;
+    private LocalDateTime modified_at;
+    private LocalDateTime deleted_at;
+}
