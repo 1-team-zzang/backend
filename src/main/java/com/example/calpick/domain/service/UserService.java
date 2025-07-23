e688280b333b1a200c58fe21d45c9aca9ba5e111
shinyurim <yoursin0330@gmail.com>
2025-07-23 13:23:53 +0900

    feat: 프로필 수정 기능

--- src/main/java/com/example/calpick/domain/service/UserService.java
+++ src/main/java/com/example/calpick/domain/service/UserService.java
@@ -2,7 +2,9 @@ package com.example.calpick.domain.service;
 
 import com.example.calpick.domain.dto.user.CustomUserDetails;
 import com.example.calpick.domain.dto.user.UserDto;
+import com.example.calpick.domain.dto.user.UserProfileRequestDto;
 
 public interface UserService {
     UserDto profile(CustomUserDetails userDetails);
+    UserDto editProfile(CustomUserDetails userDetails, UserProfileRequestDto request);
 }
--- src/main/java/com/example/calpick/domain/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/calpick/domain/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.calpick.domain.service.impl;
 
 import com.example.calpick.domain.dto.user.CustomUserDetails;
 import com.example.calpick.domain.dto.user.UserDto;
+import com.example.calpick.domain.dto.user.UserProfileRequestDto;
 import com.example.calpick.domain.entity.User;
 import com.example.calpick.domain.repository.UserRepository;
 import com.example.calpick.domain.service.UserService;
@@ -10,6 +11,7 @@ import com.example.calpick.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +24,17 @@ public class UserServiceImpl implements UserService {
         if (userDetails.getEmail() == null) throw new CalPickException(ErrorCode.UNAUTHORIZED_USER);
         User user = userRepository.findByEmail(userDetails.getEmail())
                 .orElseThrow(()->new CalPickException(ErrorCode.INVALID_EMAIL));
+        return mapper.map(user, UserDto.class);
+    }
+
+    @Override
+    @Transactional
+    public UserDto editProfile(CustomUserDetails userDetails, UserProfileRequestDto request) {
+        if (userDetails.getEmail() == null) throw new CalPickException(ErrorCode.UNAUTHORIZED_USER);
+        User user = userRepository.findByEmail(userDetails.getEmail())
+                .orElseThrow(()->new CalPickException(ErrorCode.INVALID_EMAIL));
+        user.setName(request.getName());
+        user.setProfileUrl(request.getProfileUrl());
         return mapper.map(user, UserDto.class);
     }
 }
